8c51b82e25c22ba0d462b5d776e81a51ad9aa077
Madhusudan <96856144+madhusudankulkarni@users.noreply.github.com>
2022-03-31 23:13:17 +0530

    Update: Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,53 @@
 import React from 'react';
 
-const title = 'Deep Work';
-const author = 'Cal Newport';
-const image =
-  'https://images-eu.ssl-images-amazon.com/images/I/41UMGAFVUTL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg';
+const firstBook = {
+  title: 'Deep Work',
+  author: 'Cal Newport',
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/41UMGAFVUTL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
+};
+
+const secondBook = {
+  title: 'Essentialism',
+  author: 'Greg McKeown',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/41U831OHkJL._SX322_BO1,204,203,200_.jpg',
+};
+
+const thirdBook = {
+  title: 'The One Thing',
+  author: 'Gary Keller',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/31emFJveEWL._SX341_BO1,204,203,200_.jpg',
+};
 
 function App() {
   return (
     <div>
       <section className='booklist'>
-        <Book />
-        <Book />
-        <Book />
+        <Book
+          img={firstBook.img}
+          title={firstBook.title}
+          author={firstBook.author}
+        />
+        <Book
+          img={secondBook.img}
+          title={secondBook.title}
+          author={secondBook.author}
+        />
+        <Book
+          img={thirdBook.img}
+          title={thirdBook.title}
+          author={thirdBook.author}
+        />
       </section>
     </div>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className='book'>
-      <img src={image} alt='Book Image' />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={props.img} alt='Book Image' />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
